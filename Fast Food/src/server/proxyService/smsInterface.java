f3e0dfcee57f812920260ce35a40bbb0f7d03fc0
tonyb2 <antonio.barbato@studeti.unina.it>
2015-07-12 15:02:42 +0200

    alcune modifiche ad smsInterface
    Devo creare un'altro account questo ormai ha finito gli sms gratis

--- Fast Food/src/server/proxyService/smsInterface.java
+++ Fast Food/src/server/proxyService/smsInterface.java
@@ -12,7 +12,7 @@ import com.google.gson.JsonParser;
 
 
 public class smsInterface {
-	private static String uname="xxxx@gmail.com";
+	private static String uname="psssFastFood@gmail.com";
 	private static String pword="psssFastFood10";
 	private static String from="FastFood";
 	private static String serviceUrl="https://api.txtlocal.com/send/?";
@@ -31,40 +31,8 @@ public class smsInterface {
 		conn.setDoOutput(true);											//indico che intendo utilizzare la connessione per ricevere dell'output							
 		return conn;
 	}
-		//TelephoneNumber: dovrà essere inserito con il +39 come prefisso
-	/*
-	public String sendSms(String TelephoneNumber,String Msg)  {
-		try {
-		    // Construct data
-		    String data = "uname=" + URLEncoder.encode(uname, "UTF-8");
-		    data += "&pword=" + URLEncoder.encode(pword, "UTF-8");
-		    data += "&message=" + URLEncoder.encode(Msg, "UTF-8");
-		    data += "&from=" + URLEncoder.encode(from, "UTF-8");
-		    data += "&selectednums=" + URLEncoder.encode(TelephoneNumber, "UTF-8");
-		    data += "&info=" + URLEncoder.encode("1", "UTF-8");
-		    // Send data
-		    URLConnection conn=connect();
-		    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());		//creo un output stream che sia collegato all'output stream della connessione 
-		    wr.write(data);				//scrivo i dati nello stream
-		    wr.flush();					//invio i dati nello stream
-		    // Get the response
-		    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		    String line;
-		    String sResult="";
-		    while ((line = rd.readLine()) != null) {
-		        sResult=sResult+line+" ";		// Process line...
-		    }
-		    wr.close();
-		    rd.close();
-		    return sResult;
-		} catch (Exception e) {
-		    System.out.println("Error SMS "+e);
-		    return "Error "+e;
-		}
-	}
-	*/
 	
-	
+	//TelephoneNumber: dovrà essere inserito con il +39 come prefisso
 	public Boolean sendSms(String TelephoneNumber,String Msg,int test) {
 		//test=1 indica che il messaggio non viene inviato ma viene effettuato solo un test 
 		//test=0	il messaggio viene inviato davvero
@@ -101,14 +69,7 @@ public class smsInterface {
 			    JsonObject jsonResponse=(JsonObject)jsonParser.parse(response);
 			    String status=jsonResponse.get("status").getAsString();
 			    if(status.equalsIgnoreCase("success")){
-			    	float balance=jsonResponse.get("balance").getAsFloat();
-			    	float cost=jsonResponse.get("cost").getAsFloat();
-			    	if(cost<balance){
-			    		return true;
-			    	}else{
-			    		return false;
-			    	}
-			    	//return true;
+			    	return true;
 			    }else{
 			    	return false;
 			    }
